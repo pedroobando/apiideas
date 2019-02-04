189005932c251f05ac1aa388bf688fb6957acd2c
Pedro Obando <pedro.obando@gmail.com>
2019-02-04 11:31:07 -0400

    Add modules, controller and services

---
+++ src/idea/idea.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { IdeaEntity } from './idea.entity';
+import { IdeaDTO } from './idea.dto';
+
+@Injectable()
+export class IdeaService {
+  constructor(
+    @InjectRepository(IdeaEntity)
+    private ideaRepository: Repository<IdeaEntity>,
+  ) {}
+
+  async showAll() {
+    return await this.ideaRepository.find();
+  }
+
+  async create(data: IdeaDTO) {
+    const idea = await this.ideaRepository.create(data);
+    await this.ideaRepository.save(idea);
+    return idea;
+  }
+
+  async read(id: string) {
+    const idea = await this.ideaRepository.findOne({ where: { id } });
+    Logger.log(`${idea} - ${typeof(idea)}`, 'idea.service');
+    return idea;
+  }
+
+  async update(id: string, data: Partial<IdeaDTO>) {
+    await this.ideaRepository.update({ id }, data);
+    return await this.ideaRepository.findOne({ id });
+  }
+
+  async destroy(id: string) {
+    await this.ideaRepository.delete({ id });
+    return {delete: true };
+  }
+
+}
