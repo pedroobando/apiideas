d2373660cce5d5eab7cf591b6dd0bbab650f4f7f
Pedro Obando <pedroobando@hotmail.com>
2019-02-11 16:10:51 -0400

    Add Users Modules

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,5 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, CreateDateColumn, Column, BeforeInsert } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import { UserRO } from './user.dto';
 
 @Entity('user')
 export class UserEntity {
@@ -23,8 +25,28 @@ export class UserEntity {
     this.password = await bcrypt.hash(this.password, 10);
   }
 
-  toReponseObject() {
-    const { id, created, username } = this;
-    return { id, created, username };
+  toReponseObject(showToken: boolean = true): UserRO {
+    const { id, created, username, token } = this;
+    const responseObject: any = { id, created, username };
+    if (showToken) {
+      responseObject.token = token;
+    }
+    return responseObject;
+  }
+
+  async comparePassword(attempt: string) {
+    return await bcrypt.compare(attempt, this.password);
+  }
+
+  private get token() {
+    const {id, username } = this;
+    return jwt.sign(
+      {
+        id,
+        username,
+      },
+      process.env.SECRET,
+      { expiresIn: '7d' },
+    );
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { Repository } from 'typeorm';
-import { UserDTO } from './user.dto';
+import { UserDTO, UserRO } from './user.dto';
 
 @Injectable()
 export class UserService {
@@ -10,14 +10,31 @@ export class UserService {
     @InjectRepository(UserEntity) private userRepository: Repository<UserEntity>
   ) {}
 
-  async showAll() {
+  async showAll(): Promise<UserRO[]> {
     const users = await this.userRepository.find();
-    return users.map(user => user.toReponseObject());
+    return users.map(user => user.toReponseObject(true));
   }
 
-  async login(data: UserDTO) {
+  async login(data: UserDTO): Promise<UserRO> {
     const {username, password} = data;
-    const user = await this.userRepository.find({where: {username}});
+    const user = await this.userRepository.findOne({where: {username}});
+    if (!user || !(await user.comparePassword(password))) {
+      throw new HttpException(
+        'Invalid username/password',
+        HttpStatus.BAD_REQUEST,
+      );      
+    }
+    return user.toReponseObject(true);
   }
-  register(data: UserDTO) {}
+
+  async register(data: UserDTO): Promise<UserRO> {
+    const { username } = data;
+    let user = await this.userRepository.findOne({where: {username}});
+    if (user) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    }
+    user = await this.userRepository.create(data);
+    await this.userRepository.save(user);
+    return user.toReponseObject();
+  }
 }
