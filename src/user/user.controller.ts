f3e2c559a420870540c42d1830aefbc3a67e7e8b
Pedro Obando <pedro.obando@gmail.com>
2019-02-15 12:03:28 -0400

    Add Authentication

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,9 +9,7 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Get('api/users')
-  @UseGuards(new AuthGuard())
-  showAllUsers(@User() user) {
-    console.log(user);
+  showAllUsers() {
     return this.userService.showAll();
   }
 
